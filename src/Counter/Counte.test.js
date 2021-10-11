f57a3baceca047220ef2f07ae5e3646b7d2bc6dc
Aakash <akashviahwakarma98@gmail.com>
2021-10-11 21:14:23 +0530

    Counter test suite

--- src/Counter/Counte.test.js
+++ src/Counter/Counte.test.js
@@ -10,3 +10,33 @@ test('Header renders correctly with text', () => {
 
     expect(headerEl.textContent).toBe("My Counter")
 })
+
+test('Counter initially starts with text 0', () => {
+    const {getByTestId} = render(<Counter />)
+    const counterEl = getByTestId("counter")
+
+    expect(counterEl.textContent).toBe("0")
+})
+
+test('input contains initial value of 1', () => {
+    const {getByTestId} = render(<Counter />)
+    const inputEl = getByTestId("input")
+
+    expect(inputEl.value).toBe("1")
+})
+
+test('add button renders with +', () => {
+    const {getByTestId} = render(<Counter />)
+    const addBtn = getByTestId("add-btn")
+
+    expect(addBtn.textContent).toBe("+")
+})
+
+test('Subtract button renders with -', () => {
+    const {getByTestId} = render(<Counter />)
+    const subtractBtn = getByTestId("subtract-btn")
+
+    expect(subtractBtn.textContent).toBe("-")
+})
+
+
--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Counter.css"
 
 const Counter = () => {
+
+    const [counterValue , setCounterValue] = useState(0)
+    const [inputValue , setInputValue] = useState(1)
+
     return (
         <div>
             <h3 data-testid = "header">My Counter</h3>
+            <h2 data-testid = "counter">{counterValue}</h2>
+            <button data-testid = "subtract-btn">-</button>
+            <input className = "text-center" data-testid = "input" type = "number" value = {inputValue}/>
+            <button data-testid = "add-btn">+</button>
         </div>
     )
 }
